ea35ac14abcbe7bc9d9c2a67889ce93213a3fe4f
aryan <aryan@example.com>
2026-06-28 02:20:32 +0530

    Initial commit

---
+++ server/config/database.py
@@ -0,0 +1,33 @@
+#  Below code is available in Mongo-Connect
+from pymongo.mongo_client import MongoClient
+import os
+# from os import environ as env
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the MongoDB URI using the environment variable
+uname = os.getenv("MONGODB_USERNAME")
+pwd = os.getenv("MONGODB_PASSWORD")
+
+print("uname",uname, "pwd", pwd)
+
+uri = "mongodb://localhost:27017/"
+
+
+# Create a new client to establish a connection to the MongoDB server
+client = MongoClient(uri)
+
+# Access or create a database within the MongoDB server
+db = client.url_short_db
+
+# Access or create a collection within the db database
+collection_name = db["url_short_collection"]
+
+
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
